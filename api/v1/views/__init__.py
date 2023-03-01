f6fbe15b13c2d5ded80673b82e3e8053a191c0e9
Reda <muhammadredabh@gmail.com>
2023-03-01 13:12:29 +0100

    fixed views init file to export notes route

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,3 +9,4 @@ bp = Blueprint(bp_name, __name__, url_prefix=bp_url_prefix)
 from api.v1.views.users import *
 from api.v1.views.classes import *
 from api.v1.views.announcements import *
+from api.v1.views.notes import *
